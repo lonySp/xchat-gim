f8f7a8fe9e659e27c5524ef30ae7d444776f5fe5
xiaoyuan <lony0116@163.com>
2024-11-25 20:54:09 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,30 +1,32 @@
+#!/bin/bash
+
 cd cmd/business
 rm -f business
 go build -o business main.go
 echo "打包business成功"
-pkill business
+pkill business || echo "No business process found"
 echo "停止business服务"
-nohup ./business &
+nohup ./business 2>&1 | tee ../../business.log &
 echo "启动business服务"
 
 cd ../logic
 rm -f logic
 go build -o logic main.go
 echo "打包logic成功"
-pkill logic
+pkill logic || echo "No logic process found"
 echo "停止logic服务"
-nohup ./logic &
+nohup ./logic 2>&1 | tee ../../logic.log &
 echo "启动logic服务"
 
 cd ../connect
 rm -f connect
 go build -o connect main.go
 echo "打包connect成功"
-pkill connect
+pkill connect || echo "No connect process found"
 echo "停止connect服务"
-sleep 2
-nohup ./connect &
+nohup ./connect 2>&1 | tee ../../connect.log &
 echo "启动connect服务"
+
 
 #cd ../file
 #rm -f file
